dff2c6aa0f6b8a14197ba857b133dec3a95e91ce
jvera <jvera@cifpfbmoll.eu>
2025-02-04 05:37:02 +0100

    instalada la librería iText

--- src/main/java/PrintPdfExample.java
+++ src/main/java/PrintPdfExample.java
@@ -1,3 +1,86 @@
+
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Paragraph;
+import java.io.FileNotFoundException;
+import static com.itextpdf.kernel.pdf.PdfName.Document;
+
+public class PrintPdfExample {
+    public static void main(String[] args) throws Exception {
+        try (PdfDocument pdfDoc = new PdfDocument(new PdfWriter("./hello-pdf.pdf"));
+
+             Document document = new Document(pdfDoc)) {
+            document.add(new Paragraph("Hello PDF!"));
+
+
+
+
+    /* Cuando haya que escribir
+            String dest = "/Users/sixto/Downloads/sample.pdf";
+            PdfWriter writer = new PdfWriter(dest);
+
+
+            document.close();
+            pdfDoc.addNewPage();
+
+            // Creating an Area Break
+            AreaBreak aB = new AreaBreak();
+
+            // Adding area break to the PDF
+            document.add(aB);
+
+
+             String para1 = "Tutorials Point originated from the idea that there exists
+      a class of readers who respond better to online content and prefer to learn
+      new skills at their own pace from the comforts of their drawing rooms.";
+
+      String para2 = "The journey commenced with a single tutorial on HTML in 2006
+      and elated by the response it generated, we worked our way to adding fresh
+      tutorials to our repository which now proudly flaunts a wealth of tutorials
+      and allied articles on topics ranging from programming languages to web designing
+      to academics and much more.";
+
+      // Creating Paragraphs
+      Paragraph paragraph1 = new Paragraph(para1);
+      Paragraph paragraph2 = new Paragraph(para2);
+
+      // Adding paragraphs to document
+      document.add(paragraph1);
+      document.add(paragraph2);
+
+       // Creating a table
+      float [] pointColumnWidths = {150F, 150F, 150F};
+      Table table = new Table(pointColumnWidths);
+
+      // Adding cells to the table
+      table.addCell(new Cell().add("Name"));
+      table.addCell(new Cell().add("Raju"));
+      table.addCell(new Cell().add("Id"));
+      table.addCell(new Cell().add("1001"));
+      table.addCell(new Cell().add("Designation"));
+      table.addCell(new Cell().add("Programmer"));
+
+      // Adding Table to document
+      doc.add(table);
+      */
+
+            //https://www.tutorialspoint.com/itext/itext_creating_pdf_document.htm
+            //https://www.tutorialspoint.com/itext/itext_formatting_cell_contents.htm
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+/*
 import javax.print.*;
 import javax.print.attribute.*;
 import javax.print.attribute.standard.*;
@@ -34,4 +117,4 @@ public class PrintPdfExample {
             e.printStackTrace();
         }
     }
-}
+}*/
